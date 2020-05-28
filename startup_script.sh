9979aa6614aa7524e54a4dc81a36284e529b29d1
Linus Pithan <linus.pithan@esrf.fr>
2020-05-28 15:02:43 +0200

    remove vnc

--- startup_script.sh
+++ startup_script.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 echo -e "\n------------------ bliss startup script -v3----------------"
-echo -e "\n------------------ start x1vnc----------------"
-#x11vnc -create -xkb -noxrecord -noxfixes -noxdamage -auth guess -rfbauth /etc/x11vnc.pass -rfbport 5900&
-#-findauth 
-chown nomachine /etc/x11vnc.pass
-sudo -u nomachine -E env "PATH=$PATH" /bliss_demo/vnc_startup_script.sh
 echo -e "\n------------------ start nx----------------"
 /nxserver.sh &
-echo -e "\n------------------ start nx----------------"
+echo -e "\n------------------ start bliss----------------"
 sleep 3
 mkdir -p /root/.config/glymur
 chmod 777 /root/.config/glymur
