10a67f937ada5dbb988ff601917e9d2e86f10807
Linus Pithan <linus.pithan@esrf.fr>
2020-06-02 13:31:05 +0200

    wallpaper reset

---
+++ reset_wallpaper_scaling.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo -e "\n------------------ configure desktop  ------------------"
+gsettings set org.mate.background picture-options 'zoom'
+gsettings set org.mate.background picture-options 'spanned'
