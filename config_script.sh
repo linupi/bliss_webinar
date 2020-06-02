0f55704612db60e9f0699a38309a7b7e42f77729
Linus Pithan <linus.pithan@esrf.fr>
2020-06-02 10:53:43 +0200

    add jive

--- config_script.sh
+++ config_script.sh
@@ -2,3 +2,4 @@
 echo -e "\n------------------ configure desktop  ------------------"
 gsettings set org.gnome.desktop.wm.preferences theme 'TraditionalOk'
 gsettings set org.mate.background picture-filename /bliss_webinar/assets/bliss_wallpaper.png
+gsettings set org.mate.background picture-options 'scaled'
