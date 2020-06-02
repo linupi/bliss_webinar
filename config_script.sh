bc4c754e1ed62352cb216a5b64c4964d09e9e70f
Linus Pithan <linus.pithan@esrf.fr>
2020-06-02 12:44:30 +0200

    desktop style

--- config_script.sh
+++ config_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo -e "\n------------------ configure desktop  ------------------"
-gsettings set org.gnome.desktop.wm.preferences theme 'TraditionalOk'
+gsettings set org.mate.Marco.general theme 'TraditionalOk'
+gsettings set org.mate.interface gtk-theme 'TraditionalOk'
 gsettings set org.mate.background picture-filename /bliss_webinar/assets/bliss_wallpaper.png
-gsettings set org.mate.background picture-options 'scaled'
+gsettings set org.mate.background picture-options 'spanned'
