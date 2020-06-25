e724712c7bfd12c1b6a3706fe91a53e75524754c
Linus Pithan <linus.pithan@esrf.fr>
2020-06-25 16:38:08 +0200

    lets see

--- startup_script.sh
+++ startup_script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 echo -e "\n------------------ bliss startup script -v3----------------"
+echo -e "\n------------------ start x1vnc----------------"
+#x11vnc -create -xkb -noxrecord -noxfixes -noxdamage -auth guess -rfbauth /etc/x11vnc.pass -rfbport 5900&
+#-findauth 
+chown nomachine /etc/x11vnc.pass
+sudo -u nomachine -E env "PATH=$PATH" /bliss_demo/vnc_startup_script.sh
 echo -e "\n------------------ start nx----------------"
 /nxserver.sh &
+echo -e "\n------------------ start novnc----------------"
+websockify -D --web=/usr/share/novnc/ 6080 localhost:5900 &
 echo -e "\n------------------ start bliss----------------"
 sleep 3
 mkdir -p /root/.config/glymur
