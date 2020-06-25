ca98c1b825f1ed4a3f9d1d68e46d465e1ea73617
Linus Pithan <linus.pithan@esrf.fr>
2020-06-25 23:51:34 +0200

    5

--- vnc_startup_script.sh
+++ vnc_startup_script.sh
@@ -6,9 +6,9 @@ COOKIE=`ps -ef | md5sum | cut -f 1 -d " "`
 AUTHFILE=$HOME/Xvfb-0.auth
 xauth -f $AUTHFILE add :0 MIT-MAGIC-COOKIE-1 $COOKIE
 xauth add :0 MIT-MAGIC-COOKIE-1 $COOKIE
-Xvfb :0 -auth $AUTHFILE -screen 0 1600x900x24 &
+Xvfb :0 -auth $AUTHFILE -screen 0 1280x720x24 &
 DISPLAY=:0 nohup /etc/X11/Xsession default &
 sleep 2
-DISPLAY=:0 x11vnc -xkb -noxrecord -rfbauth /etc/x11vnc.pass -rfbport 5900 &
+DISPLAY=:0 x11vnc -xkb -noxrecord -rfbauth /etc/x11vnc.pass -rfbport 5900 -forever &
 
 #-noxdamage
