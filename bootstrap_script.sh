61731fd5fa9400c17229e3039a5d477ca63ddd5d
Linus Pithan <linus.pithan@esrf.fr>
2020-06-25 23:12:41 +0200

    3

--- bootstrap_script.sh
+++ bootstrap_script.sh
@@ -3,6 +3,7 @@
 echo -e "\n------------------ bootstrap script --v1----------------"
 cd /bliss_webinar
 git fetch
+git checkout vnc
 git reset --hard origin/vnc
 cd ../bliss
 git fetch
--- startup_script.sh
+++ startup_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "\n------------------ bliss startup script -v3----------------"
+echo -e "\n------------------ bliss startup script -v4----------------"
 echo -e "\n------------------ start x1vnc----------------"
 chown nomachine /etc/x11vnc.pass
-sudo -u nomachine -E env "PATH=$PATH" /bliss_demo/vnc_startup_script.sh
+sudo -u nomachine -E env "PATH=$PATH" /bliss_webinar/vnc_startup_script.sh &
 echo -e "\n------------------ start nx----------------"
 /nxserver.sh &
 echo -e "\n------------------ start novnc----------------"
